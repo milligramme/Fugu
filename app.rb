d76a613a979639ef2f1eb6d34747ec4e2784b896
milligramme <milligramme.cc@gmail.com>
2011-06-27 23:54:55 +0900

    add to views

---
+++ app.rb
@@ -0,0 +1,15 @@
+get '/' do
+  haml :index
+end
+
+get '/metamo' do
+  haml :metamo
+end  
+
+get '/heart' do
+  haml :hearts
+end  
+
+get '/moji' do
+  haml :moji
+end  
---
+++ config.ru
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require "bundler"
+
+Bundler.require
+
+require "./app.rb"
+
+run Sinatra::Application
